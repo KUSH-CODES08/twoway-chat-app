2c251830eb9d4689c7b639a0f712a100be52b255
KUSH-CODES08 <bhavsarkush78@gmail.com>
2026-02-17 16:05:58 +0530

    SQL ---> Railway mysql

--- src/main/java/com/chat/util/DBConnection.java
+++ src/main/java/com/chat/util/DBConnection.java
@@ -5,14 +5,16 @@ import java.sql.DriverManager;
 
 public class DBConnection {
 
+    private static final String URL =
+        "jdbc:mysql://metro.proxy.rlwy.net:40384/railway?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+
+    private static final String USER = "root";
+    private static final String PASSWORD = "AQbokmKcqMRqziqEyuInFrQObqNAFePt";  // your railway password
+
     public static Connection getConnection() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/chatapp",
-                "root",
-                "kush_08_03@"
-            );
+            return DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (Exception e) {
             e.printStackTrace();
             return null;
